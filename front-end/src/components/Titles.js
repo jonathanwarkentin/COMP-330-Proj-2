2594c7eb6929b5205db2789d234dff8905d8e129
Jonathan <warkentin.jon@gmail.com>
2019-02-27 01:20:26 -0600

    modify main description, change font weight 600 to 700 globally

--- front-end/src/components/Titles.js
+++ front-end/src/components/Titles.js
@@ -4,8 +4,8 @@ const Titles = () => (
   <div>
     <h1 id="main-title">TWEETTRENDS</h1>
     <h2 id="main-description">
-      This app displays sentiment analyses and other information of trending
-      topics and users on Twitter.
+      Sentiment analyses and other information for the top 10 trending topics on
+      Twitter
     </h2>
   </div>
 );
